3e5c49fdc480889df5e6a9de13d326e27bddd1e1
Allas Assis <allasassis10@gmail.com>
2024-02-04 18:02:55 -0300

    Changes in the terminal command

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import subprocess
 import time
 
-
 def open_terminal(command):
-    terminal_command = f"cd C:\kafka && {command}"
+    terminal_command = rf"cd C:\kafka && {command}"
     subprocess.Popen(["start", "cmd", "/K", terminal_command], shell=True)
-
 
 zookeeper_command = r".\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties"
 kafka_command = r".\bin\windows\kafka-server-start.bat .\config\server.properties"
 
 open_terminal(zookeeper_command)
-time.sleep(20)
+time.sleep(12)
 open_terminal(kafka_command)
