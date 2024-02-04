2361302c5ec3cbb861df90fa3794189dde51f023
Allas Assis <allasassis10@gmail.com>
2024-02-04 17:50:32 -0300

    Prompt created

---
+++ main.py
@@ -0,0 +1,15 @@
+import subprocess
+import time
+
+
+def open_terminal(command):
+    terminal_command = f"cd C:\kafka && {command}"
+    subprocess.Popen(["start", "cmd", "/K", terminal_command], shell=True)
+
+
+zookeeper_command = r".\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties"
+kafka_command = r".\bin\windows\kafka-server-start.bat .\config\server.properties"
+
+open_terminal(zookeeper_command)
+time.sleep(20)
+open_terminal(kafka_command)
